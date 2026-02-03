f936d94004b69216755379f21cc37daa28af7633
Luc Boily <luc.boily@gmail.com>
2026-02-02 22:39:59 -0500

    ajout garage

--- components/GarageRecruitmentForm.tsx
+++ components/GarageRecruitmentForm.tsx
@@ -158,6 +158,9 @@ function Input({
         {...props}
         className="w-full rounded-lg border border-slate-300 px-4 py-3 outline-none focus:ring-2 focus:ring-blue-600"
       />
+	   <p className="mt-10 text-center text-sm uppercase tracking-wide text-gray-400">
+      Soumissions-Auto.ca • 2026
+    </p>
     </div>
   )
 }
