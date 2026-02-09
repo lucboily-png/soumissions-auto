68f2204434dc6a1daa3f0d5719c83c08e87341a8
Luc Boily <luc.boily@gmail.com>
2026-02-09 14:53:39 -0500

    Fix Stripe runtime nodejs

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
-import Stripe from 'stripe'
 export const runtime = 'nodejs'
-
+import Stripe from 'stripe'
 export async function POST(req: Request) {
   try {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
