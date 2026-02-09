92291177747673d6a35a4af2e55906c29da84a52
Luc Boily <luc.boily@gmail.com>
2026-02-09 00:21:54 -0500

    ajout garage

--- app/api/stripe/checkout/route.ts
+++ app/api/stripe/checkout/route.ts
@@ -1,6 +1,6 @@
-import Stripe from 'stripe'
 import { NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
+import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2026-01-28.clover',
